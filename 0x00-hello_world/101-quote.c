da84ba6a6b2ae6a3adbe1bcd74fd647660a8898d
chidozieamah <chidozieamah@gmail.com>
2022-03-12 03:34:58 +0100

    okay

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 1 (Error)
-*/
+ * main - prints exactly and that
+ * piece of art is useful" - Dora Korpar, 2015-10-19,
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\"");
-	fprintf(stderr, "- Dora Korpar, 2015-10-19\n", 58);
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
