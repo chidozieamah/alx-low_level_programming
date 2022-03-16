1b25a6223162bd2dfbe00e2beba65dd1f397ee49
chidozieamah <chidozieamah@gmail.com>
2022-03-16 16:57:47 +0100

    nested loop

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: nothing, return void
+ */
+int main(void)
+
+{
+int i; /*Declaring statements*/
+char string[8] = "_putchar"; /*Arrays*/
+
+for (i = 0 ; i < 8 ; i++) /*Start for*/
+{
+_putchar(string[i]);/*display word*/
+} /*End for*/
+
+_putchar('\n');
+
+return (0);
+
+}
